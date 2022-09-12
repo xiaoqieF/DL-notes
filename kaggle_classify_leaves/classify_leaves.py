4ce1419b8cc21951d942be5c531ac8edc556972b
xiaoqieF <fanxiaodong@buaa.edu.cn>
2022-09-12 20:26:19 +0800

    [fix] add kaggle-classify-leaves

---
+++ kaggle_classify_leaves/classify_leaves.py
@@ -0,0 +1,88 @@
+"""
+kaggle: https://www.kaggle.com/competitions/classify-leaves/overview
+you should download data by yourself, ant put it in '../data/'
+"""
+import torch
+from PIL import Image
+import os
+from torch.utils import data
+import torchvision
+from util import *
+from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
+
+writer = SummaryWriter("log/ResNet")
+
+train_csv = pd.read_csv("../data/classify-leaves/train.csv")
+test_csv = pd.read_csv("../data/classify-leaves/test.csv")
+
+leaves_labels = sorted(list(set(train_csv['label'])))
+n_classes = len(leaves_labels)
+class_to_num = dict(zip(leaves_labels, range(n_classes)))
+num_to_class = {v: k for k, v in class_to_num.items()}
+
+transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomRotation(degrees=90),
+        torchvision.transforms.ColorJitter(0.5, 0.5),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+transform_test = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+class LeavesDataset(data.Dataset):
+    def __init__(self, train_csv, train=True, ratio=0.2, transform=None):
+        super(LeavesDataset, self).__init__()
+        self.trans = transform
+        self.train_csv = train_csv
+        if train:
+            self.data = self.train_csv[:int(self.train_csv.shape[0] * (1 - ratio))]
+        else:
+            self.data = self.train_csv[int(self.train_csv.shape[0] * (1 - ratio)):]
+        print(self.data.shape)
+
+    def __getitem__(self, index):
+        label = class_to_num[self.data.iloc[index, 1]]
+        image = Image.open(os.path.join('../data/classify-leaves', self.data.iloc[index, 0]))
+
+        if self.trans:
+            image = self.trans(image)
+        return [image, label]
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+def predict(net, test_csv):
+    print(test_csv)
+    net.eval()
+    with open("../data/classify-leaves/sub.csv", 'w') as f:
+        f.write('image,label\n')
+        for path in test_csv.loc[:, 'image']:
+            img = Image.open(os.path.join('../data/classify-leaves', path))
+            img = transform_test(img).unsqueeze(0).to(try_gpu(0))
+            with torch.no_grad():
+                y = net(img).cpu()
+                print(num_to_class[y.argmax(dim=1).item()])
+                f.write(path + ',' + num_to_class[y.argmax(dim=1).item()] + '\n')
+
+
+if __name__ == '__main__':
+    train_iter = data.DataLoader(LeavesDataset(train_csv, train=True, transform=transform_train),
+                                 batch_size=16, shuffle=True, num_workers=6, drop_last=True)
+    test_iter = data.DataLoader(LeavesDataset(train_csv, train=False, transform=transform_test),
+                                batch_size=16, shuffle=False, num_workers=6, drop_last=False)
+    net = torchvision.models.resnet18(pretrained=True)
+    net.fc = nn.Sequential(
+        nn.Linear(net.fc.in_features, 512), nn.ReLU(), nn.Dropout(0.5), nn.Linear(512, 176))
+    print(net)
+    train_net_multi_gpu(net, train_iter, test_iter, 100, 0.01, 2, writer=writer)
+    torch.save(net.state_dict(), 'model/net.params5')
+    # net.load_state_dict(torch.load('model/net.params4'))
+    # train_net_multi_gpu(net, train_iter, test_iter, 40, 0.05, 2)
+    # net.to(try_gpu(0))
+    # predict(net, pd.read_csv('../data/classify-leaves/test.csv'))
---
+++ kaggle_classify_leaves/util.py
@@ -0,0 +1,110 @@
+"""
+train and evaluate net
+"""
+
+import torch
+from torch import nn
+
+
+def try_gpu(i=0):
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+
+def accuracy(y_hat, y):
+    """
+    return sum of predict-true labels
+    """
+    y_hat = y_hat.argmax(axis=1)
+    return (y_hat.type(y.dtype) == y).sum()
+
+
+def evaluate_accuracy(net, data_iter):
+    """
+    evaluate accuracy of net in data_iter
+    device depends on net
+    """
+    net.eval()
+    dev = next(iter(net.parameters())).device
+    true_nums, total_nums = 0, 0
+    with torch.no_grad():
+        for x, y in data_iter:
+            x, y = x.to(dev), y.to(dev)
+            true_nums += accuracy(net(x), y)
+            total_nums += y.numel()
+    return true_nums / total_nums
+
+
+def init_weight(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        nn.init.xavier_uniform_(m.weight)
+
+
+def print_res(epoch, train_acc, test_acc, avg_loss, writer=None):
+    if writer is not None:
+        writer.add_scalar("loss", avg_loss, global_step=epoch)
+        writer.add_scalars("Acc", tag_scalar_dict={
+            'train_acc': train_acc,
+            'test_acc': test_acc
+        }, global_step=epoch)
+    print(f'epoch:{epoch}, loss:{avg_loss}, '
+          f'train_acc:{train_acc}, test_acc:{test_acc}')
+
+
+def train_net(net, train_iter, test_iter, num_epochs, lr, device, writer=None):
+    """
+    train and evaluate net on device
+    """
+    # net.apply(init_weight)
+    print('training on', device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 4, 0.9)
+    loss = nn.CrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        net.train()
+        train_true_nums, train_total_nums, train_loss = 0, 0, 0
+        for x, y in train_iter:
+            optimizer.zero_grad()
+            x, y = x.to(device), y.to(device)
+            y_hat = net(x)
+            ls = loss(y_hat, y)
+            ls.backward()
+            optimizer.step()
+            train_true_nums += accuracy(y_hat, y)
+            train_total_nums += y.numel()
+            train_loss += ls * y.numel()
+        scheduler.step()
+        test_acc = evaluate_accuracy(net, test_iter)
+        print_res(epoch, train_true_nums/train_total_nums, test_acc, train_loss / train_total_nums, writer)
+    print("finish")
+
+
+def train_net_multi_gpu(net, train_iter, test_iter, num_epochs, lr, num_gpus, writer=None):
+    # net.apply(init_weight)
+    devices = [try_gpu(i) for i in range(num_gpus)]
+    net = nn.DataParallel(net, device_ids=devices)
+    net.to(devices[0])
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.00001)
+    # optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=0.0001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 4, 0.9)
+    loss = nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        net.train()
+        train_true_nums, train_total_nums, train_loss = 0, 0, 0
+        for x, y in train_iter:
+            optimizer.zero_grad()
+            x, y = x.to(devices[0]), y.to(devices[0])
+            y_hat = net(x)
+            ls = loss(y_hat, y)
+            ls.backward()
+            optimizer.step()
+            train_true_nums += accuracy(y_hat, y)
+            train_total_nums += y.numel()
+            train_loss += ls * y.numel()
+        test_acc = evaluate_accuracy(net, test_iter)
+        scheduler.step()
+        print_res(epoch, train_true_nums/train_total_nums, test_acc, train_loss / train_total_nums, writer)
+    print("finish")
